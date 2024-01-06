220688c143b239c258ffe5605991dcf5d7b03e5a
codeswithroh <codeswithroh@gmail.com>
2024-01-06 16:45:15 +0530

    linking is almost done

---
+++ index.js
@@ -0,0 +1,20 @@
+var donateButton = document.getElementById("donate-button");
+var donateButtonHeader = document.getElementById("donate-button-header");
+var linkedinButton = document.getElementById("linkedin-button");
+var youtubeButton = document.getElementById("youtube-button");
+
+donateButton.addEventListener("click", function () {
+  window.open("https://www.buymeacoffee.com/codeswithroh", "_blank");
+});
+
+donateButtonHeader.addEventListener("click", function () {
+  window.open("https://www.buymeacoffee.com/codeswithroh", "_blank");
+});
+
+linkedinButton.addEventListener("click", function () {
+  window.open("https://www.linkedin.com/in/codeswithroh/", "_blank");
+});
+
+youtubeButton.addEventListener("click", function () {
+  window.open("https://www.youtube.com/@rohitpurkait6951", "_blank");
+});
